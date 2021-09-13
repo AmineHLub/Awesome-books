046604cd4285950aea24c5fd2e60609da0a57bb5
AmineHLub <Amino.bhl@gmail.com>
2021-09-13 15:07:12 +0100

    Add button stores data in array of objects and show them back dynamically

--- script.js
+++ script.js
@@ -9,9 +9,37 @@ class BooksAndAuthor {
   }
 }
 
+const arrOfBooks = [];
+
+// eslint-disable-next-line consistent-return
 function addBook() {
-  const bookAndAuthor = new BooksAndAuthor(title, author);
-  console.log('ok');
+  arrOfBooks.push(new BooksAndAuthor(title.value, author.value));
+  if (!title.value || !author.value) {
+    if (!document.querySelector('.adding-form > span')) {
+      const errorMsg = document.createElement('span');
+      errorMsg.innerText = 'Please fill the fields before adding your book!';
+      document.querySelector('.adding-form').append(errorMsg);
+    }
+  } else {
+    if (document.querySelector('.adding-form > span')) {
+      document.querySelector('.adding-form > span').remove();
+    }
+    const div = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const h3 = document.createElement('h3');
+    const button = document.createElement('button');
+    div.classList.add('book');
+    h2.classList.add('book-name');
+    h3.classList.add('author');
+    button.classList.add('remove');
+    document.querySelector('.books-list').append(div);
+    h2.innerText = title.value;
+    h3.innerText = author.value;
+    button.innerText = 'Remove';
+    div.append(h2);
+    div.append(h3);
+    div.append(button);
+  }
 }
 
 add.addEventListener('click', addBook);
