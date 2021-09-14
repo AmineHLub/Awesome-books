ffe33bce8b6be3c176243c6b373a8ef480386aa0
AmineHLub <Amino.bhl@gmail.com>
2021-09-14 16:06:32 +0100

    Add 2nd class to the first project which already has one
    Second class if for data management(has no constructor)
    
    Add adding function and display items on reload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-implied-eval */
+/* eslint-disable no-useless-constructor */
+/* eslint-disable max-classes-per-file */
 const add = document.querySelector('#add_button');
 const author = document.getElementById('author');
 const title = document.getElementById('title');
@@ -10,72 +14,73 @@ class BooksAndAuthor {
   }
 }
 
-let arrOfBooks;
+class Management {
+  add() {
+    let arrOfBooks = [];
+    this.previousData = JSON.parse(localStorage.getItem('storedBooks'));
+    if (this.previousData) {
+      arrOfBooks = this.previousData;
+    }
+    const idGenerator = Math.floor(Math.random() * 100);
+    arrOfBooks.push(new BooksAndAuthor(title.value, author.value, idGenerator));
+    localStorage.setItem('storedBooks', JSON.stringify(arrOfBooks));
+    console.log(arrOfBooks);
+    // eslint-disable-next-line no-restricted-globals
+    setTimeout('window.location.reload()', 0);
+  }
 
-arrOfBooks = JSON.parse(localStorage.getItem('storedBooks'));
-if (arrOfBooks === null) {
-  arrOfBooks = [];
-}
-
-// elements of array are posted on load
-if (arrOfBooks.length > 0) {
-  for (let i = 0; i < arrOfBooks.length; i += 1) {
-    const div = document.createElement('div');
-    const h2 = document.createElement('h2');
-    const h3 = document.createElement('h3');
-    const buttonContainer = document.createElement('div');
-    div.classList.add('book');
-    div.id = `${arrOfBooks[i].idOfBook}`;
-    h2.classList.add('book-name');
-    h3.classList.add('author');
-    document.querySelector('.books-list').append(div);
-    h2.innerText = arrOfBooks[i].book;
-    h3.innerText = arrOfBooks[i].author;
-    div.append(h2);
-    div.append(h3);
-    buttonContainer.innerHTML = `<button onclick="arrposition(${arrOfBooks[i].idOfBook})">Remove</button>`;
-    div.append(buttonContainer);
+  delete(number) {
+    document.getElementById(`${number}`).remove();
   }
 }
 
-// eslint-disable-next-line consistent-return
-function addBook() {
+// eslint-disable-next-line no-unused-vars
+function addbook() {
   if (!title.value || !author.value) {
-    if (!document.querySelector('.adding-form > span')) {
-      const errorMsg = document.createElement('span');
+    if (!document.querySelector('.adding-form > div')) {
+      const errorMsg = document.createElement('div');
       errorMsg.innerText = 'Please fill the fields before adding your book!';
       document.querySelector('.adding-form').append(errorMsg);
     }
   } else {
-    if (document.querySelector('.adding-form > span')) {
-      document.querySelector('.adding-form > span').remove();
+    if (document.querySelector('.adding-form > div')) {
+      document.querySelector('.adding-form > div').remove();
     }
-    const idGenerator = Math.floor(Math.random() * 100);
-    arrOfBooks.push(new BooksAndAuthor(title.value, author.value, idGenerator));
-    const div = document.createElement('div');
-    const h2 = document.createElement('h2');
-    const h3 = document.createElement('h3');
-    const buttonContainer = document.createElement('div');
-    div.classList.add('book');
-    div.id = `${idGenerator}`;
-    h2.classList.add('book-name');
-    h3.classList.add('author');
-    document.querySelector('.books-list').append(div);
-    h2.innerText = title.value;
-    h3.innerText = author.value;
-    div.append(h2);
-    div.append(h3);
-    buttonContainer.innerHTML = `<button onclick="arrposition(${idGenerator})">Remove</button>`;
-    div.append(buttonContainer);
-    localStorage.setItem('storedBooks', JSON.stringify(arrOfBooks));
+    new Management().add();
   }
 }
 
-add.addEventListener('click', addBook);
-
 // eslint-disable-next-line no-unused-vars
-function arrposition(number) {
-  document.getElementById(`${number}`).remove();
-  arrOfBooks.splice(arrOfBooks.findIndex((item) => item.idOfBook === number), 1);
-  localStorage.setItem('storedBooks', JSON.stringify(arrOfBooks));
+function helperDelete(number) {
+  new Management().delete(number);
 }
+
+function showbooklist() {
+  const storageData = JSON.parse(localStorage.getItem('storedBooks'));
+  if (storageData) {
+    for (let i = 0; i < storageData.length; i += 1) {
+      const div = document.createElement('div');
+      const h2 = document.createElement('h2');
+      const h3 = document.createElement('h3');
+      const buttonContainer = document.createElement('div');
+      div.classList.add('book');
+      div.id = `${storageData[i].idOfBook}`;
+      h2.classList.add('book-name');
+      h3.classList.add('author');
+      document.querySelector('.books-list').append(div);
+      h2.innerText = storageData[i].book;
+      h3.innerText = storageData[i].author;
+      div.append(h2);
+      div.append(h3);
+      buttonContainer.innerHTML = `<button onclick="helperDelete(${storageData[i].idOfBook})">Remove</button>`;
+      div.append(buttonContainer);
+    }
+  } else {
+    const errorMsg = document.createElement('span');
+    errorMsg.innerText = 'There are no books saved!';
+    document.querySelector('.adding-form').append(errorMsg);
+  }
+}
+
+showbooklist();
+add.addEventListener('click', addbook);
